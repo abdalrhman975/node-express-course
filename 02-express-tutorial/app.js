a13a40543c89a0fe660edbe0dd48459de13a3e2e
Abdalrhman Obeid <abdalrhmanobeid19@gmail.com>
2024-02-23 18:53:28 +0300

    first comit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,10 @@
 const http = require( 'http' );
 
-const server = http.createServer((req , res)=>{
-    console.log( 'user hit the servre' );
-    res.end('Home Page');
-} )
+const server = http.createServer( ( req, res ) =>
+{
+    res.writeHead( 200, { 'content-type': 'text/html' } )
+    console.log( req.method );
+    res.write( '<h1>home page</h1>' )
+    res.end()
+} );
 server.listen( 5000 );
